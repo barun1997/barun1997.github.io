e39ce3de8d3adc80bcd3fd6430d7a17eb791b4c9
barun1997 <barun.pandey@icloud.com>
2020-02-19 10:07:11 +0545

    initialized my portfolio

---
+++ src/pages/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles/app.css'; 
+
+
+export default function App() { 
+    return ( 
+        <div className='container'> 
+            <p className='header'> 
+                Hello world from my home in the internet.
+            </p>
+            <p className='sub-header'>
+                My home is currently on construction. Stay tuned! 
+            </p>
+        </div>
+    )
+}
